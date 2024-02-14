403aa230b61c5f6a7d0d87fea7ae5d34fe734b8c
Sergio Florez <sergio.j.dev@gmail.com>
2024-02-13 21:37:08 -0500

    arreglado problema tiempo ejecucion

--- recursivo.c
+++ recursivo.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <sys/time.h>
+#include <time.h>
 
 long long factorial(int n) {
     if (n == 0 || n == 1)
@@ -13,21 +13,21 @@ int main() {
     printf("Enter a number to calculate its factorial: ");
     scanf("%d", &num);
 
-    struct timeval start_time, end_time;
-    gettimeofday(&start_time, NULL); // Get start time
+    struct timespec start_time, end_time;
+    clock_gettime(CLOCK_REALTIME, &start_time); // Get start time
 
     long long result = factorial(num);
 
-    gettimeofday(&end_time, NULL); // Get end time
+    clock_gettime(CLOCK_REALTIME, &end_time); // Get end time
 
     // Calculate elapsed time in microseconds
-    long long start_microseconds = start_time.tv_sec * 1000000 + start_time.tv_usec;
-    long long end_microseconds = end_time.tv_sec * 1000000 + end_time.tv_usec;
-    long long elapsed_microseconds = end_microseconds - start_microseconds;
+    long long start_nanoseconds = start_time.tv_sec * 1000000000 + start_time.tv_nsec;
+    long long end_nanoseconds = end_time.tv_sec * 1000000000 + end_time.tv_nsec;
+    long long elapsed_nanoseconds = end_nanoseconds - start_nanoseconds;
+    double elapsed_microseconds = (double)elapsed_nanoseconds / 1000.0;
 
     printf("Factorial of %d is %lld\n", num, result);
-    printf("Execution time: %lld microseconds\n", elapsed_microseconds);
+    printf("Execution time: %.6f microseconds\n", elapsed_microseconds);
 
     return 0;
 }
- 
